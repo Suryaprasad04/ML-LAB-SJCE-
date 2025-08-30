b0540ea430cf1f33e3cf485d811cbe24dc649dbb
SURYAPRASAD L <150020783+Suryaprasad04@users.noreply.github.com>
2025-08-30 12:40:13 +0530

    Update and rename seventhB.py to Lab-07.py

---
+++ Lab-07.py
@@ -0,0 +1,13 @@
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
+import pandas as pd
+data=pd.read_csv('glass.csv')
+
+X=data.drop('Type', axis=1)
+y=data['Type']
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
+model=KMeans(n_clusters=3)
+model.fit(X_train)
+y_pred=model.predict(X_test)
+print("Accuracy:",accuracy_score(y_test,y_pred))
