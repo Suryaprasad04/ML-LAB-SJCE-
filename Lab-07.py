ad011c18b36b0682c8770c344b72dd055bcb17c7
SURYAPRASAD L <150020783+Suryaprasad04@users.noreply.github.com>
2025-08-31 21:18:43 +0530

    Update Lab-07.py

--- Lab-07.py
+++ Lab-07.py
@@ -1,4 +1,4 @@
-from sklearn.cluster import KMeans
+from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 import pandas as pd
@@ -7,7 +7,7 @@ data=pd.read_csv('glass.csv')
 X=data.drop('Type', axis=1)
 y=data['Type']
 X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.3,random_state=42)
-model=KMeans(n_clusters=3)
-model.fit(X_train)
+model=KNeighborsClassifier(n_neighbors=3)
+model.fit(X_train,y_train)
 y_pred=model.predict(X_test)
 print("Accuracy:",accuracy_score(y_test,y_pred))
